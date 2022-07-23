61981f2df8e1db5490a1adce8aea9d146edfd57b
Rushiahire <rushiahire9567@gmail.com>
2022-07-23 14:05:06 +0530

    video background

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,13 @@ import video from '../components/video/video.mp4';
 const Home = () => {
     return(
         <>
-            <video src={video} autoPlay loop muted
-            className='background-video'/>
-
-            <div className="content">
-                Hello ,
-                 My name is Rushikesh Ahire
-            </div>
+        <div className='video-part'>
+        <video src={video} autoPlay loop muted
+            className='background-video' id='overlay'/>
+    <div>
+            
+    </div>
+        </div>
         </>
     )
 
